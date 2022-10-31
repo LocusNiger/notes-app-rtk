57c122c36cf0006e0bc41deaa7e9481af3d7e0b4
Iván Sánchez <sanchezivan.798@gmail.com>
2022-10-31 01:10:08 -0300

    Feat: Add Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import TaskForm from "./components/taskForm";
 import TaskList from "./components/taskList";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<TaskList />} />
           <Route path="/create-task" element={<TaskForm />} />
           <Route path="/edit-task/:id" element={<TaskForm />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
